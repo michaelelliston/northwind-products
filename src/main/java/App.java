5d5651c5fec9404690f777c152712b937d2e671a
Michael <michael.elliston95@gmail.com>
2025-12-01 12:14:54 -0600

    Refactored all methods to use BasicDataSource instead of DriverManager, and added a menu selection for Display all categories, with implemented code.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,18 +1,28 @@
+import org.apache.commons.dbcp2.BasicDataSource;
 import utilities.InputGetter;
 
+import javax.sql.DataSource;
+import javax.xml.transform.Result;
 import java.sql.*;
 
 public class App {
 
-    private static final String DB_USERNAME = "root";
-    private static final String DB_PASSWORD = "yearup";
+    private static final String[] USER_INFO = {"root", "yearup"};
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/northwind";
 
     public static void main(String[] args) {
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(DB_URL);
+        dataSource.setUsername(USER_INFO[0]);
+        dataSource.setPassword(USER_INFO[1]);
+
         int userSelection = 99;
         while (userSelection != 0) {
             userSelection = InputGetter.getInt("""
                     1) Display all products
                     2) Display all customers
+                    3) Display all categories
                     
                     0) Exit
                     
@@ -20,112 +30,84 @@ public class App {
                     """);
 
             switch (userSelection) {
-                case 1 -> displayProductsFromNorthwind();
-                case 2 -> displayCustomersOfNorthwind();
+                case 1 -> displayProductsFromNorthwind(dataSource);
+                case 2 -> displayCustomersOfNorthwind(dataSource);
+                case 3 -> displayCategoriesOfNorthwind(dataSource);
             }
         }
     }
 
-    public static void displayProductsFromNorthwind() {
+    private static void displayCategoriesOfNorthwind(DataSource dataSource) {
 
-        ResultSet resultSet = null;
-        Statement statement = null;
-        Connection connection = null;
-        try {
+        try (Connection connection = dataSource.getConnection()) {
 
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", DB_USERNAME, DB_PASSWORD);
-            statement = connection.createStatement();
+            PreparedStatement preparedStatement = connection.prepareStatement("select CategoryID, CategoryName from categories order by CategoryID;");
 
-            resultSet = statement.executeQuery("select * from products;");
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    System.out.printf("Category ID: %d | Category Name: %15s\n", resultSet.getInt(1), resultSet.getString(2));
+                }
+            }
 
-            while (resultSet.next()) {
-                String name = resultSet.getString("ProductName");
-                int productID = resultSet.getInt("ProductID");
-                double unitPrice = resultSet.getDouble("UnitPrice");
-                int unitsStocked = resultSet.getInt("UnitsInStock");
-                System.out.printf("%30s | ProductID: %3d | Unit Price: $%.2f | Units Stocked: %3d\n", name, productID, unitPrice, unitsStocked);
+            int userInput = InputGetter.getInt("\nPlease input the CategoryID number you wish to see products of: ");
+
+            preparedStatement = connection.prepareStatement("select ProductID, ProductName, UnitPrice, UnitsInStock from products where CategoryID = ?");
+            preparedStatement.setInt(1, userInput);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    System.out.printf("Product ID: %3d | Product Name: %40s | Unit Price: $%.2f | Units in Stock: %3d\n",
+                            resultSet.getInt(1), resultSet.getString(2), resultSet.getDouble(3), resultSet.getInt(4));
+                }
             }
 
             InputGetter.getString("\nPlease input any character to continue: ");
             System.out.println();
-            connection.close();
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    System.err.println("An error occurred: " + e);
-                }
-            }
-            if (statement != null) {
-                try {
-                    statement.close();
-                } catch (SQLException e) {
-                    System.err.println("An error occurred: " + e);
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    System.err.println("An error occurred: " + e);
-                }
-            }
+            System.err.println("An error occurred: " + e);
         }
     }
 
-    public static void displayCustomersOfNorthwind() {
+    public static void displayProductsFromNorthwind(DataSource dataSource) {
 
-        ResultSet resultSet = null;
-        Statement statement = null;
-        Connection connection = null;
-        try {
+        try (Connection connection = dataSource.getConnection()) {
 
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", DB_USERNAME, DB_PASSWORD);
-            statement = connection.createStatement();
+            PreparedStatement preparedStatement = connection.prepareStatement("select ProductName, ProductID, UnitPrice, UnitsInStock from products;");
 
-            resultSet = statement.executeQuery("select * from customers order by country;");
-
-            while (resultSet.next()) {
-                String contactName = resultSet.getString("ContactName");
-                String companyName = resultSet.getString("CompanyName");
-                String city = resultSet.getString("City");
-                String country = resultSet.getString("Country");
-                String phoneNumber = resultSet.getString("Phone");
-                System.out.printf("Contact Name: %30s | Company Name: %40s | City: %15s | Country: %15s | Phone Number: %15s\n", contactName, companyName, city, country, phoneNumber);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    System.out.printf("%30s | ProductID: %3d | Unit Price: $%.2f | Units Stocked: %3d\n", resultSet.getString(1), resultSet.getInt(2),
+                            resultSet.getDouble(3), resultSet.getInt(4));
+                }
             }
 
             InputGetter.getString("\nPlease input any character to continue: ");
             System.out.println();
-            connection.close();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        } finally {
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    System.err.println("An error occurred: " + e);
+        }
+    }
+
+    public static void displayCustomersOfNorthwind(DataSource dataSource) {
+
+        try (Connection connection = dataSource.getConnection()) {
+
+            PreparedStatement preparedStatement = connection.prepareStatement("select ContactName, CompanyName, City, Country, Phone from customers;");
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    System.out.printf("Contact Name: %30s | Company Name: %40s | City: %15s | Country: %15s | Phone Number: %15s\n",
+                            resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5));
                 }
             }
-            if (statement != null) {
-                try {
-                    statement.close();
-                } catch (SQLException e) {
-                    System.err.println("An error occurred: " + e);
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    System.err.println("An error occurred: " + e);
-                }
-            }
+
+            InputGetter.getString("\nPlease input any character to continue: ");
+            System.out.println();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
